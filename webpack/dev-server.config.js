b571f5b9c159b723b972b953e9e19ef76c13202e
robcmills <robcmills@gmail.com>
2017-01-21 14:00:26 -0800

    Silence webpack warnings.

--- webpack/dev-server.config.js
+++ webpack/dev-server.config.js
@@ -8,34 +8,37 @@ var publicPath = commonConfig.output.publicPath
 // var externals = commonConfig.externals
 
 module.exports = {
-    // The configuration for the server-side rendering
-    name: 'server-side rendering',
-    context: path.join(__dirname, '..', 'app'),
-    entry: {
-      server: '../server/index'
-    },
-    target: 'node',
-    // node: {
-    //   __dirname: false
-    // },
-    // devtool: 'sourcemap',
-    output: {
-      // The output directory as absolute path
-      path: path.join(__dirname, '..', 'compiled'),
-      // The filename of the entry chunk as relative path inside the output.path directory
-      filename: '[name].dev.js',
-      // The output path from the view of the Javascript
-      // publicPath: publicPath,
-      // libraryTarget: 'commonjs2'
-    },
-    module: {
-      loaders: commonLoaders
-    },
-    // resolve: {
-    //   root: [path.join(__dirname, '..', 'app')],
-    //   extensions: ['', '.js', '.jsx'],
-    // },
-    plugins: [
-        new webpack.EnvironmentPlugin(['NODE_ENV']),
-    ],
+  // The configuration for the server-side rendering
+  name: 'server-side rendering',
+  context: path.join(__dirname, '..', 'app'),
+  entry: {
+    server: '../server/index'
+  },
+  target: 'node',
+  // node: {
+  //   __dirname: false
+  // },
+  // devtool: 'sourcemap',
+  output: {
+    // The output directory as absolute path
+    path: path.join(__dirname, '..', 'compiled'),
+    // The filename of the entry chunk as relative path inside the output.path directory
+    filename: '[name].dev.js',
+    // The output path from the view of the Javascript
+    // publicPath: publicPath,
+    // libraryTarget: 'commonjs2'
+  },
+  module: {
+    loaders: commonLoaders
+  },
+  // resolve: {
+  //   root: [path.join(__dirname, '..', 'app')],
+  //   extensions: ['', '.js', '.jsx'],
+  // },
+  plugins: [
+      new webpack.EnvironmentPlugin(['NODE_ENV']),
+  ],
+  stats: {
+    warnings: false
+  }
 }
